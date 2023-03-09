90a99bb888b4f9183821aae003c2d069df5b9a41
Lewis-McKenzie <mckenzielewis39@gmail.com>
2023-03-09 13:52:15 +0000

    validation update

--- validation/validate.py
+++ validation/validate.py
@@ -1,12 +1,20 @@
 
+FILEPATH = "/home/userfs/l/lm1797/Assessment/vortex-shedding/out/vortex.vtk"
 
 class validator:
-    def __init__(self) -> None:
-        pass
+    @staticmethod
+    def read_file(filepath: str) -> None:
+        with open(filepath, 'r') as file:
+            text = file.read().replace("\n", " ")
+            tables = text.split("LOOKUP_TABLE default")
+            print(tables[0])
+            for table in tables[1:]:
+                print(table)
+
 
 
 def main() -> None:
-    pass
+    validator.read_file(FILEPATH)
 
 if __name__ == "__main__":
     main()
