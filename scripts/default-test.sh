13607d08aaf232d032050fcaa58d25ffcbf3aee5
Lewis-McKenzie <mckenzielewis39@gmail.com>
2023-04-06 17:27:56 +0100

    updated benchmark

--- scripts/default-test.sh
+++ scripts/default-test.sh
@@ -1,12 +1,14 @@
 #! /usr/bin/bash
 dir=$(pwd)
 
-rm -r ./obj
+make clean
 make
 
-bench='benchmarks/main/default'
-test='benchmarks/main/default'
+bench_dir='benchmarks/main/default'
+test_dir='benchmarks/main/default'
 
-./vortex -d 0.0025 -o $test
+mkdir -p $dir/$test_dir
 
-python3 ./validation/validate.py $dir/$bench.vtk $dir/$test.vtk
+./vortex -d 0.0025 -o $test_dir/vortex 2>&1 | tee $dir/$test_dir/output.log
+
+python3 ./validation/validate.py $dir/$bench_dir/vortex.vtk $dir/$test_dir/vortex.vtk
