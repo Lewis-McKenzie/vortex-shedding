d2a214a3a4d8006d16c8a99f4203ae67a02bc593
Lewis-McKenzie <mckenzielewis39@gmail.com>
2023-04-18 15:16:45 +0100

    update job

--- scripts/profile-test.sh
+++ scripts/profile-test.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/bash
+dir=$(pwd)
+
+bench='benchmarks/main/default'
 
 make clean
 make
 
 nvprof ./vortex -d 0.0025
+
+python3 ./validation/validate.py $dir/$bench/vortex.vtk $dir/out/vortex.vtk
